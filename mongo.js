4da9e66d548fa1dd0d3444dabef59d6c1ef396dd
Hadi Arbabi <h.arbabi@banicm.com>
2020-04-14 13:44:59 +0430

    useFullQuery

---
+++ mongo.js
@@ -0,0 +1,2 @@
+db.getCollection('irancell_sdp_sub_history_4').aggregate([{ "$group": { _id: "$from", "ev": { "$last": "$$ROOT" }, count: { $sum: 1 } } }, { $sort: { "count": -1 } }])
+//group data by phone numbers and count by phone numbers and show the last event type
